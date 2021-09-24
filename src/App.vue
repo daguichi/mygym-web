33d424b6edd93975ea3cf2d79b035170c872ca4a
robrodriguez99 <64867075+robrodriguez99@users.noreply.github.com>
2021-09-24 09:45:18 -0300

    armando la home

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,93 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar
+        app
+        color="blue lighten-3"
+
+    >
+<!-- aca arriba podemos agregarle la siguiente cosa pero queda muy grueso al principio:  shrink-on-scroll-->
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
 
       <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
+      <v-row>
+        <v-col>
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+
+          elevation="2"
+          rounded
+          x-large
+          outlined
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        home
+        <v-icon>
+          right
+          mdi-home</v-icon>
+      </v-btn>
+        </v-col>
+        <v-col>
+      <v-btn
+          elevation="2"
+          rounded
+          outlined
+          x-large
+
+      >
+        rutinas
+        <v-icon>
+          right
+          mdi-weight-lifter</v-icon>
+      </v-btn>
+        </v-col>
+        <v-col>
+      <v-btn
+          elevation="2"
+          rounded
+          outlined
+          x-large
+      >
+        favoritos
+        <v-icon>
+          right
+          mdi-heart
+        </v-icon>
+      </v-btn>
+        </v-col>
+      </v-row>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-btn
+      x-large>
+        <v-icon>
+          left
+          mdi-account-circle
+        </v-icon>
+        username
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+    <home-page></home-page>
     </v-main>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
 
+<script>
+import HomePage from "./components/homePage";
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
+  components: {HomePage},
+  theme: {
+    themes: {
+      light: {
+        primary: '#a1d7d3'
+      },
+    },
   },
-
   data: () => ({
     //
   }),
