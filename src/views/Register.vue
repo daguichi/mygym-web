9535d5f817427c1d23c83552a81c8f6776e9cbe8
ssandrini <ssandrini@itba.edu.ar>
2021-09-27 00:11:03 -0300

    Register falta cambiar el fondo y logo

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="bg">
+  <div class="bg fivepeople " >
     <v-container fluid class="fill-height pa-0 ma-0">
-      <v-row align="center" justify="center" style="height: 250px">
+      <v-row align="center" justify="center" >
             <v-img
-            lazy-src="https://picsum.photos/id/11/10/6"
             max-height="150"
             max-width="250"
-            src="https://picsum.photos/id/11/500/300"
+            src="../assets/MyGymLogo.png"
             ></v-img>
       </v-row>
       <v-row>
         <v-col>
-          <v-row justify="center" align="center" style="height: 250px">
-            <v-card rounded color="#92D9D3" elevation="0">
+          <v-row justify="center" align="center">
+            <v-card class="rounded-xl" rounded color="#92D9D3" elevation="1">
               <v-container class="account-icon">
                 <v-row justify="center" aligh= "center">
                     <v-icon color="#475dff" dense large x-large>mdi-account-circle</v-icon>
@@ -23,18 +22,69 @@
                   <v-row>
                     <v-text-field
                       background-color="rgb(244, 249, 252)"
-                      label="asdasd"
+                      placeholder="Usuario"
                       elevation="2"
-                      color="grey"
+                      color="black"
                       dense
-                      required
                       filled
-                      dark
-                      x-large
                       rounded
-                      class="mr-3"
-                      v-model="verificationCode"
-                      @keyup.enter="verificarCodigo"
+                      placeholderColor="#aaa"
+                      textAlign="left"
+                      textColor="#333"
+                      truncate={false}
+                      value=""
+                      x-large
+                    ></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field
+                      background-color="rgb(244, 249, 252)"
+                      placeholder="Correo electrónico"
+                      elevation="2"
+                      color="black"
+                      dense
+                      filled
+                      rounded
+                      placeholderColor="#aaa"
+                      textAlign="left"
+                      textColor="#333"
+                      truncate={false}
+                      value=""
+                      x-large
+                    ></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field
+                      background-color="rgb(244, 249, 252)"
+                      placeholder="Contraseña"
+                      elevation="2"
+                      color="black"
+                      dense
+                      filled
+                      rounded
+                      placeholderColor="#aaa"
+                      textAlign="left"
+                      textColor="#333"
+                      truncate={false}
+                      value=""
+                      x-large
+                    ></v-text-field>
+                  </v-row>
+                  <v-row>
+                    <v-text-field
+                      background-color="rgb(244, 249, 252)"
+                      placeholder="Confirmar contraseña"
+                      elevation="2"
+                      color="black"
+                      dense
+                      filled
+                      rounded
+                      placeholderColor="#aaa"
+                      textAlign="left"
+                      textColor="#333"
+                      truncate={false}
+                      value=""
+                      x-large
                     ></v-text-field>
                   </v-row>
                   <v-row class="text-center" justify="center">
@@ -71,5 +121,15 @@ export default {
 <style scoped>
 .account-icon {
     margin-top: 10px;
+}
+.fivepeople {
+  width: auto;
+  height: 700px;
+  overflow: visible;
+  opacity: 1;
+  background-image: url('https://media.istockphoto.com/photos/group-of-five-peoples-in-silhouettes-at-sunset-picture-id899192146?k=20&m=899192146&s=612x612&w=0&h=tcCIoXgXjbjFsJnqV26-HGwCfDG0GGyB0X0vseAZNVg=');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 </style>
