0ba13895d3247b6ee66884cf6a33a64e0f6f93c4
sol <skonfederak@itba.edu.ar>
2021-10-07 17:57:43 -0300

    tratando de arreglar router con clase12 y boton goback

---
+++ src/components/GoBack.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="go-back">
+    <button @click="goBack" class="btn">Go Back</button>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "GoBack",
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.go-back {
+  display: flex;
+  cursor: pointer;
+}
+button {
+  border: 0;
+}
+</style>
--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,15 +1,20 @@
 <template>
   <v-app>
-      <navBar actualPage="favoritos"></navBar>
+    <navBar actualPage="favoritos"></navBar>
+    <br><br><br><br>
+    <GoBack />
       <!--      no se pq pero hay que meter un br aca pq sino la lavbar tapa la palabra destacados-->
   </v-app>
 </template>
 
 <script>
 import navBar from "../components/navBar";
+import GoBack from "@/components/GoBack";
+
 export default {
   name: "Favorites",
-  components:{navBar}
+  components:{navBar,
+  GoBack}
 }
 </script>
 
