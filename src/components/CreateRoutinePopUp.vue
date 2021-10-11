efec3ad9b3807d243ad73ab985bcda25bf5cc842
franco rodriguez tulasne <robrodriguez@itba.edu.ar>
2021-10-11 15:23:39 -0300

    create routine almost ready

--- src/components/CreateRoutinePopUp.vue
+++ src/components/CreateRoutinePopUp.vue
@@ -79,6 +79,7 @@
             ></v-col>
           </v-row>
           <v-row>
+            <!-- me va a servir para el stepper -->
             <v-card-text>
               <v-select
                 v-model="steps"
@@ -99,12 +100,285 @@
                 Cancelar
               </v-btn>
             </v-col>
-            <v-col>
-              <v-btn @click="createRoutine" color="primary">
-                crear rutina
-              </v-btn>
-            </v-col>
-            <!-- siguiente paso -->
+            <v-col> </v-col>
+            <!-- siguiente paso, comienzan los ciclos-->
+            <v-dialog
+              v-model="createRoutineDialogStep2"
+              persistent
+              max-width="900px"
+              transition="v-slide-y-transition"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" dark v-bind="attrs" v-on="on"
+                  >Siguiente</v-btn
+                >
+              </template>
+
+              <!-- calentamiento step -->
+              <v-card class="rounded-xl">
+                <v-stepper v-model="e1">
+                  <v-stepper-header>
+                    <v-stepper-step
+                      :key="`calentamiento-step`"
+                      :step="0"
+                      editable
+                    >
+                      Calentamiento
+                    </v-stepper-step>
+                    <!-- n step -->
+                    <v-divider v-if="n !== steps" :key="n"></v-divider>
+                    <template v-for="n in steps">
+                      <v-stepper-step :key="`${n}-step`" :step="n" editable>
+                        {{ n }}º Ciclo
+                      </v-stepper-step>
+                      <v-divider v-if="n !== steps" :key="n"></v-divider>
+                    </template>
+                    <v-divider v-if="n !== steps" :key="n"></v-divider>
+                    <v-stepper-step
+                      :key="`enfriemiento-step`"
+                      :step="steps + 1"
+                      editable
+                    >
+                      Enfriamiento
+                    </v-stepper-step>
+                  </v-stepper-header>
+                  <!-- calentamiento content  -->
+                  <v-stepper-items>
+                    <v-stepper-content :key="`calentamiento-content`" :step="0">
+                      <v-card
+                        class="mb-12"
+                        color="grey lighten-1"
+                        height="200px"
+                        elevation="0"
+                      >
+                        <v-list
+                          two-line
+                          style="max-height:250px"
+                          class="overflow-y-auto"
+                        >
+                          <v-list-item-group
+                            v-model="selected[0]"
+                            active-class="grey--text"
+                            multiple
+                            ><template v-for="(excersise, index) in excersises">
+                              <v-row v-bind:key="excersise.id">
+                                <v-col>
+                                  <v-list-item :key="excersise.id">
+                                    <template v-slot:default="{ active }">
+                                      <v-list-item-content>
+                                        <v-list-item-title
+                                          v-text="excersise.nombre"
+                                        ></v-list-item-title>
+                                      </v-list-item-content>
+                                      <v-list-item-action>
+                                        <v-icon
+                                          v-if="active"
+                                          color="grey lighten-1"
+                                        >
+                                          mdi-check
+                                        </v-icon>
+                                      </v-list-item-action>
+                                    </template>
+                                  </v-list-item>
+                                  <v-divider
+                                    v-if="index < excersise.length - 1"
+                                    :key="index"
+                                  ></v-divider>
+                                </v-col>
+
+                                <v-col>
+                                  <v-select
+                                    v-model="reps"
+                                    :items="repeticiones"
+                                    label="repeticiones"
+                                    outlined
+                                    rounded
+                                    dense
+                                    item-text="show"
+                                    item-value="value"
+                                    :menu-props="{ maxHeight: '400' }"
+                                  ></v-select>
+                                </v-col>
+                                <v-col>
+                                  <v-select
+                                    v-model="segs"
+                                    :items="segundos"
+                                    label="segundos"
+                                    outlined
+                                    rounded
+                                    dense
+                                    item-text="show"
+                                    item-value="value"
+                                    :menu-props="{ maxHeight: '400' }"
+                                  ></v-select>
+                                </v-col> </v-row></template
+                          ></v-list-item-group>
+                        </v-list>
+                      </v-card>
+                    </v-stepper-content>
+
+                    <!-- n content  -->
+                    <v-stepper-content
+                      v-for="n in steps"
+                      :key="`${n}-content`"
+                      :step="n"
+                    >
+                      <v-card
+                        class="mb-12"
+                        color="grey lighten-1"
+                        height="200px"
+                        elevation="0"
+                      >
+                        <v-list
+                          two-line
+                          style="max-height:250px"
+                          class="overflow-y-auto"
+                        >
+                          <v-list-item-group
+                            v-model="selected[n]"
+                            active-class="grey--text"
+                            multiple
+                          >
+                            <template v-for="(excersise, index) in excersises">
+                              <v-row v-bind:key="excersise.id">
+                                <v-col>
+                                  <v-list-item :key="excersise.id">
+                                    <template v-slot:default="{ active }">
+                                      <v-list-item-content>
+                                        <v-list-item-title
+                                          v-text="excersise.nombre"
+                                        ></v-list-item-title>
+                                      </v-list-item-content>
+                                      <v-list-item-action>
+                                        <v-icon
+                                          v-if="active"
+                                          color="grey lighten-1"
+                                        >
+                                          mdi-check
+                                        </v-icon>
+                                      </v-list-item-action>
+                                    </template>
+                                  </v-list-item>
+                                  <v-divider
+                                    v-if="index < excersise.length - 1"
+                                    :key="index"
+                                  ></v-divider>
+                                </v-col>
+                                <v-col>
+                                  <v-select
+                                    v-model="reps"
+                                    :items="repeticiones"
+                                    label="repeticiones"
+                                    outlined
+                                    rounded
+                                    dense
+                                    item-text="show"
+                                    item-value="value"
+                                    :menu-props="{ maxHeight: '400' }"
+                                  ></v-select>
+                                </v-col>
+                                <v-col>
+                                  <v-select
+                                    v-model="segs"
+                                    :items="segundos"
+                                    label="segundos"
+                                    outlined
+                                    rounded
+                                    dense
+                                    item-text="show"
+                                    item-value="value"
+                                    :menu-props="{ maxHeight: '400' }"
+                                  ></v-select>
+                                </v-col>
+                              </v-row>
+                            </template>
+                          </v-list-item-group>
+                        </v-list>
+                      </v-card>
+                    </v-stepper-content>
+                    <!-- enfriamiento content -->
+                    <v-stepper-content
+                      :key="`enfriamiento-content`"
+                      :step="steps + 1"
+                    >
+                      <v-card
+                        class="mb-12"
+                        color="grey lighten-1"
+                        height="200px"
+                        elevation="0"
+                      >
+                        <v-list
+                          two-line
+                          style="max-height:250px"
+                          class="overflow-y-auto"
+                        >
+                          <v-list-item-group
+                            v-model="selected[steps + 1]"
+                            active-class="grey--text"
+                            multiple
+                          >
+                            <template v-for="(excersise, index) in excersises">
+                              <v-row v-bind:key="excersise.id">
+                                <v-col>
+                                  <v-list-item :key="excersise.id">
+                                    <template v-slot:default="{ active }">
+                                      <v-list-item-content>
+                                        <v-list-item-title
+                                          v-text="excersise.nombre"
+                                        ></v-list-item-title>
+                                      </v-list-item-content>
+                                      <v-list-item-action>
+                                        <v-icon
+                                          v-if="active"
+                                          color="grey lighten-1"
+                                        >
+                                          mdi-check
+                                        </v-icon>
+                                      </v-list-item-action>
+                                    </template>
+                                  </v-list-item>
+                                  <v-divider
+                                    v-if="index < excersise.length - 1"
+                                    :key="index"
+                                  ></v-divider>
+                                </v-col>
+                                <v-col>
+                                  <v-select
+                                    v-model="reps"
+                                    :items="repeticiones"
+                                    label="repeticiones"
+                                    outlined
+                                    rounded
+                                    dense
+                                    item-text="show"
+                                    item-value="value"
+                                    :menu-props="{ maxHeight: '400' }"
+                                  ></v-select>
+                                </v-col>
+                                <v-col>
+                                  <v-select
+                                    v-model="segs"
+                                    :items="segundos"
+                                    label="segundos"
+                                    outlined
+                                    rounded
+                                    dense
+                                    item-text="show"
+                                    item-value="value"
+                                    :menu-props="{ maxHeight: '400' }"
+                                  ></v-select>
+                                </v-col>
+                              </v-row>
+                            </template>
+                          </v-list-item-group>
+                        </v-list>
+                      </v-card>
+                    </v-stepper-content>
+                  </v-stepper-items>
+                </v-stepper>
+                <v-btn @click="createRoutineDialogStep2 = false"> Atras</v-btn>
+              </v-card>
+            </v-dialog>
           </v-row>
         </v-card-text>
       </v-card>
@@ -125,6 +399,52 @@ export default {
       { show: "Intermedio", value: "intermediate" },
       { show: "Avanzado", value: "advanced" },
       { show: "Experto", value: "expert" },
+    ],
+    reps: "-",
+    repeticiones: [
+      5,
+      10,
+      15,
+      20,
+      25,
+      30,
+      35,
+      40,
+      45,
+      50,
+      55,
+      60,
+      65,
+      70,
+      75,
+      80,
+      85,
+      90,
+      95,
+      100,
+    ],
+    segs: "-",
+    segundos: [
+      5,
+      10,
+      15,
+      20,
+      25,
+      30,
+      35,
+      40,
+      45,
+      50,
+      55,
+      60,
+      65,
+      70,
+      75,
+      80,
+      85,
+      90,
+      95,
+      100,
     ],
     col: "Azul",
     colores: [
