09a63d4ba02ff5b224d95a1bbfdff10fb18981bf
franco rodriguez tulasne <robrodriguez@itba.edu.ar>
2021-10-14 00:16:40 -0300

    agrande iconos

--- src/components/InfoRoutine.vue
+++ src/components/InfoRoutine.vue
@@ -1,84 +1,86 @@
 <template>
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on, attrs }">
-                    <v-btn @click="getCycles" icon color="primary" dark v-bind="attrs" v-on="on">
-                      <v-icon>mdi-information</v-icon>
-                    </v-btn>
-      </template>
-      <v-card flat>
-        
-        <v-card-title
-          >{{ rutina.name }} <v-spacer></v-spacer>
-          <v-btn @click="close" color="#6262f8" outlined>
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-subtitle></v-card-subtitle>
-        <v-card-subtitle class="black--text"
-          ><span class="font-weight-bold">Descripción: </span
-          >{{ rutina.detail }}</v-card-subtitle
-        >
-        <v-card-subtitle class="black--text"
-          ><span class="font-weight-bold">Dificultad: </span
-          >{{ translateDifficulty(rutina.difficulty) }}</v-card-subtitle>
-        
-        <h4 class="mt-4 pl-6 mb-4">Ciclos</h4>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        @click="getCycles"
+        icon
+        color="primary"
+        dark
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon x-large>mdi-information</v-icon>
+      </v-btn>
+    </template>
+    <v-card flat>
+      <v-card-title
+        >{{ rutina.name }} <v-spacer></v-spacer>
+        <v-btn @click="close" color="#6262f8" outlined>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-subtitle></v-card-subtitle>
+      <v-card-subtitle class="black--text"
+        ><span class="font-weight-bold">Descripción: </span
+        >{{ rutina.detail }}</v-card-subtitle
+      >
+      <v-card-subtitle class="black--text"
+        ><span class="font-weight-bold">Dificultad: </span
+        >{{ translateDifficulty(rutina.difficulty) }}</v-card-subtitle
+      >
 
-        <v-expansion-panels v-for="(ciclo, i) in this.ciclos" :key="ciclo.id">
-          <v-expansion-panel>
-            <v-expansion-panel-header >
-              {{ ciclo.name }}
-            </v-expansion-panel-header>
-            
-            <v-expansion-panel-content >
-              
-              <v-chip class="mb-4 ma-2" dark small color="#2679CC"
-                >{{ ciclo.repetitions }}</v-chip
-              >
-            
-              
-              <template v-for="ejs in cycleExercises[i]">
-                <v-card small class="mt-1" :key="ejs.exercise.id">
-                  <v-row>
-                    <v-col>
-                      <v-card-text>{{ ejs.exercise.name }}</v-card-text>
-                    </v-col>
-                    <v-col> </v-col>
-                    <v-spacer></v-spacer>
-                    <v-col>
-                      <v-card-text> Duración: {{ ejs.duration }}</v-card-text>
-                    </v-col>
-                    <v-col>
-                      <v-card-text>
-                        Repeticiones: {{ ejs.repetitions }}</v-card-text
-                      >
-                    </v-col>
-                  </v-row>
-                </v-card>
-              </template>
-            
-              
-            </v-expansion-panel-content>
-            
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </v-card>
-    </v-dialog>
+      <h4 class="mt-4 pl-6 mb-4">Ciclos</h4>
+
+      <v-expansion-panels v-for="(ciclo, i) in this.ciclos" :key="ciclo.id">
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            {{ ciclo.name }}
+          </v-expansion-panel-header>
+
+          <v-expansion-panel-content>
+            <v-chip class="mb-4 ma-2" dark small color="#2679CC">{{
+              ciclo.repetitions
+            }}</v-chip>
+
+            <template v-for="ejs in cycleExercises[i]">
+              <v-card small class="mt-1" :key="ejs.exercise.id">
+                <v-row>
+                  <v-col>
+                    <v-card-text>{{ ejs.exercise.name }}</v-card-text>
+                  </v-col>
+                  <v-col> </v-col>
+                  <v-spacer></v-spacer>
+                  <v-col>
+                    <v-card-text> Duración: {{ ejs.duration }}</v-card-text>
+                  </v-col>
+                  <v-col>
+                    <v-card-text>
+                      Repeticiones: {{ ejs.repetitions }}</v-card-text
+                    >
+                  </v-col>
+                </v-row>
+              </v-card>
+            </template>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-import {mapActions, mapState} from "vuex"
+import { mapActions, mapState } from "vuex";
 export default {
   data: () => ({
     dialog: false,
-    cycleExercises: [ [], [], [], [], [], [] ]
+    cycleExercises: [[], [], [], [], [], []],
   }),
   props: {
     rutina: Object,
   },
   computed: {
-    ...mapState("cycle", { ciclos: state => state.cycle}),
+    ...mapState("cycle", { ciclos: (state) => state.cycle }),
     isPrivate() {
       if (!this.showUsername) {
         return !this.rutina.isPublic;
@@ -88,10 +90,10 @@ export default {
     },
   },
   methods: {
-    cancelActionRut: function () {
+    cancelActionRut: function() {
       this.$store.dispatch("changeCardID"); //es como un flag que avisa un cambio de estado
     },
-    translateDifficulty: function (difficulty) {
+    translateDifficulty: function(difficulty) {
       if (difficulty == "rookie") {
         return "Novato";
       }
@@ -109,19 +111,20 @@ export default {
       }
     },
     ...mapActions("cycle", { $getCycles: "getAll" }),
-    ...mapActions("cycleExercise", { $getCycleExercises: "getAll"}),
+    ...mapActions("cycleExercise", { $getCycleExercises: "getAll" }),
     close() {
       this.dialog = false;
-    },  
+    },
     async getCycles() {
-      await this.$getCycles(this.rutina.id)
-      
-      for(let i = 0; i < this.ciclos.length; i++) {
-        this.cycleExercises[i] = await this.$getCycleExercises(this.ciclos[i].id);
+      await this.$getCycles(this.rutina.id);
+
+      for (let i = 0; i < this.ciclos.length; i++) {
+        this.cycleExercises[i] = await this.$getCycleExercises(
+          this.ciclos[i].id
+        );
         console.log(this.cycleExercises[i]);
       }
-      
     },
-  },    
+  },
 };
 </script>
