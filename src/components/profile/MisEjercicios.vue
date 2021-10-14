12631711d60156e937960ac75c85186a7ecc9a42
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-10-14 16:59:06 -0300

    v-else en mis rutinas y mis ejercicios

--- src/components/profile/MisEjercicios.vue
+++ src/components/profile/MisEjercicios.vue
@@ -1,16 +1,35 @@
 <template>
   <v-col>
     <v-sheet max-height="100vh" rounded="lg">
-      <h1 class="pt-8 pb-16 font-weight-bold ">Mis ejercicios</h1>
-      <v-carousel dark>
-        <v-carousel-item
-          class="mx-auto"
-          v-for="my in this.myExercises"
-          :key="my.id"
-        >
-          <ExerciseCard v-bind:exercise="my" />
-        </v-carousel-item>
-      </v-carousel>
+      <h1 class="pa-6 font-weight-bold">Mis ejercicios</h1>
+      <v-container v-if="this.myExercises.length > 0">
+        <v-carousel dark height="320">
+          <v-carousel-item
+            class="mx-auto"
+            v-for="my in this.myExercises"
+            :key="my.id"
+          >
+            <ExerciseCard v-bind:exercise="my" />
+          </v-carousel-item>
+        </v-carousel>
+      </v-container>
+      <v-container v-else>
+        <v-col>
+          <v-row>
+              <v-icon class="icon" color="#6262f8" x-large>mdi-weight-lifter</v-icon>
+          </v-row>
+          <v-row class= "pad" align="center" justify="center"><h3>No tienes ejercicios creados</h3></v-row>
+          <v-row class= "pad" align="center" justify="center"><h5>Ve al inicio y toca en el boton + para crear ejercicios</h5></v-row>
+          <v-row class= "pad" align="center" justify="center">
+            <router-link to="/home" style="text-decoration: none">
+              <v-btn color="#6262f8" elevation="2" rounded outlined x-large>
+                <v-icon> right mdi-home</v-icon>
+                Inicio
+              </v-btn>
+            </router-link>
+          </v-row>
+        </v-col>
+      </v-container>
     </v-sheet>
   </v-col>
 </template>
@@ -38,4 +57,12 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.pad{
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+.icon{
+  margin: auto;
+}
+</style>
