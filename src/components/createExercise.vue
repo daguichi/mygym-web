e0391a1e0207d1ee1846eb14117a85b144b9c108
franco rodriguez tulasne <robrodriguez@itba.edu.ar>
2021-10-14 18:14:12 -0300

    maxlenght para los campos de createExercise

--- src/components/createExercise.vue
+++ src/components/createExercise.vue
@@ -12,9 +12,11 @@
       <v-card-text>
         <v-container>
           <v-row>
-            <v-col >
+            <v-col>
               <v-text-field
                 label="Nombre*"
+                maxlength="25"
+                counter
                 v-model="exName"
                 :rules="rules.name"
                 required
@@ -32,16 +34,18 @@
                 item-value="value"
                 :rules="rules.ex"
                 :menu-props="{ maxHeight: '400' }"
-                >
+              >
               </v-select>
             </v-col>
-            </v-row>
-            <v-row>
+          </v-row>
+          <v-row>
             <v-col cols="12">
               <v-text-field
                 :rules="rules.desc"
                 v-model="exDesc"
                 label="Descripcion*"
+                maxlength="200"
+                counter
                 required
               ></v-text-field>
             </v-col>
@@ -52,33 +56,30 @@
       <v-card-actions>
         <v-row>
           <v-col>
-            <v-btn color="#6262f8" outlined @click="exerciseDialog = false; clearFields();">
+            <v-btn
+              color="#6262f8"
+              outlined
+              @click="
+                exerciseDialog = false;
+                clearFields();
+              "
+            >
               Cancelar
             </v-btn>
           </v-col>
           <v-col>
-            <v-btn
-              :disabled="validFields"
-              color="primary"
-              @click="save"
-            >
+            <v-btn :disabled="validFields" color="primary" @click="save">
               Guardar
             </v-btn>
           </v-col>
         </v-row>
       </v-card-actions>
     </v-card>
-    <v-snackbar
-        v-model="snackbar"
-        color="success"
-    ><v-icon class="save">mdi-check</v-icon>
+    <v-snackbar v-model="snackbar" color="success"
+      ><v-icon class="save">mdi-check</v-icon>
       Ejercicio agregado correctamente
       <template v-slot:action="{ attrs }">
-        <v-btn
-            text
-            v-bind="attrs"
-            @click="close"
-        >
+        <v-btn text v-bind="attrs" @click="close">
           Cerrar
         </v-btn>
       </template>
@@ -95,18 +96,36 @@ export default {
     exName: "",
     exDesc: "",
     exType: "",
-    types: [{ show: "Ejercicio", value: "exercise" },
-        { show: "Descanso", value: "rest" }],
+    types: [
+      { show: "Ejercicio", value: "exercise" },
+      { show: "Descanso", value: "rest" },
+    ],
     exerciseDialog: false,
     rules: {
-      name: [(val) =>  (val || "").length > 0 || val === undefined || "Campo obligatorio"],
-      desc: [(val) =>  (val || "").length > 0 || val === undefined || "Campo obligatorio"],
-      ex: [(val) =>  (val || "").length > 0 || val === undefined || "Campo obligatorio"],
+      name: [
+        (val) =>
+          (val || "").length > 0 || val === undefined || "Campo obligatorio",
+      ],
+      desc: [
+        (val) =>
+          (val || "").length > 0 || val === undefined || "Campo obligatorio",
+      ],
+      ex: [
+        (val) =>
+          (val || "").length > 0 || val === undefined || "Campo obligatorio",
+      ],
     },
   }),
   computed: {
     validFields() {
-      return this.exName === "" || this.exName === undefined || this.exDesc === "" || this.exDesc === undefined || this.exType==="" || this.exType=== undefined;
+      return (
+        this.exName === "" ||
+        this.exName === undefined ||
+        this.exDesc === "" ||
+        this.exDesc === undefined ||
+        this.exType === "" ||
+        this.exType === undefined
+      );
     },
   },
   methods: {
@@ -115,22 +134,27 @@ export default {
     async save() {
       let user = await this.$getUser();
       let userId = { id: user.id };
-      let newEx = {name: this.exName, detail: this.exDesc, type: this.exType, metadata: userId }
+      let newEx = {
+        name: this.exName,
+        detail: this.exDesc,
+        type: this.exType,
+        metadata: userId,
+      };
       await this.$createExercise(newEx);
       this.snackbar = true;
     },
     clearFields() {
-      this.exName = undefined
-      this.exDesc = undefined
-      this.exType = undefined
+      this.exName = undefined;
+      this.exDesc = undefined;
+      this.exType = undefined;
     },
     close() {
       this.clearFields();
       this.exerciseDialog = false;
-      this.snackbar=false
-    }
+      this.snackbar = false;
+    },
   },
-  
+
   /*
   name*	string
   maxLength: 100
