b2857b662953a3fe1a0ec06c4f549da1c6ef3779
Santiago Sandrini <ssandrini@itba.edu.ar>
2021-10-14 00:00:46 -0300

    perfil casi completo

--- src/components/profile/MisRutinas.vue
+++ src/components/profile/MisRutinas.vue
@@ -1,41 +1,17 @@
 <template>
   <v-col>
-    <v-sheet min-height="100vh" rounded="lg">
+    <v-sheet max-height="100vh" rounded="lg">
       <h1 class="pa-6 font-weight-bold">Mis rutinas</h1>
-     
-      <v-card elevation="24" max-width="444" class="mx-auto">
-        <v-system-bar lights-out></v-system-bar>
-        <v-carousel
-          :continuous="false"
-          :cycle="cycle"
-          :show-arrows="false"
-          hide-delimiter-background
-          delimiter-icon="mdi-minus"
-          height="300"
-        >
+        <v-carousel dark height="300">
           <v-carousel-item v-for="my in this.myRoutines"
             :key="my.id">
           
             <RoutineCard
               v-bind:routine="my"
             />
-            <v-list two-line>
-              <v-list-item>
-                <v-list-item-avatar>
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/john.png"
-                  ></v-img>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title>John Leider</v-list-item-title>
-                  <v-list-item-subtitle>Author</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
+            
           </v-carousel-item>
         </v-carousel>
-      </v-card>
-      
     </v-sheet>
   </v-col>
 </template>
@@ -62,5 +38,6 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
 </style>
